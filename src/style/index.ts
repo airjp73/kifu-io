0808d1581addf2939777547ddbd6fd804d88fe2c
airjp73 <aaron.pettengill.violin@gmail.com>
2019-02-17 18:07:04 -0500

    Add base style variables

---
+++ src/style/index.ts
@@ -0,0 +1,8 @@
+export const panelBackground = '#3E4E88';
+export const highlight = '#DBF4FF';
+export const primaryAction = '#7971EA';
+export const fadedGrey = '#6D6D6D';
+export const error = '#E8291D';
+export const dark = '#1A2C5B';
+
+export const panelHover = 'rgba(1, 1, 1, .25)';
