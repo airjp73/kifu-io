4b8ee21a5e434dd60e48c6d02004685eb83db52a
airjp73 <aaron.pettengill.violin@gmail.com>
2019-02-18 18:51:23 -0500

    Add two test files

---
+++ src/components/Header.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-testing-library';
+import FontIcon from './FontIcon';
+
+test('should render a span with the `material-icons` class and any provided classes', () => {
+  const rendered = render(<FontIcon icon="person" className="test-class" />);
+  const icon = rendered.getByText('person');
+  expect(icon.classList).toContain('material-icons');
+  expect(icon.classList).toContain('test-class');
+});
