b26447ea36bdfd702c87600a5ce874b76fb41ae1
airjp73 <aaron.pettengill.violin@gmail.com>
2019-03-01 21:48:43 -0500

    Add control buttons

---
+++ src/components/GameControlButtons.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import styled from 'styled-components';
+import { useGoGameContext } from 'contexts/GoGameContext';
+import FontIcon from './FontIcon';
+import FlatButton from './FlatButton';
+
+const GameControlButtonBar = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  color: black;
+  height: 35px;
+`;
+
+const GameControlButtons = () => {
+  const { forward, back } = useGoGameContext();
+
+  return (
+    <GameControlButtonBar>
+      <FlatButton onClick={() => back(-1)}>
+        <FontIcon icon="skip_previous" />
+      </FlatButton>
+      <FlatButton onClick={() => back(10)}>
+        <FontIcon icon="fast_rewind" />
+      </FlatButton>
+      <FlatButton onClick={() => back(1)}>
+        <FontIcon icon="navigate_before" />
+      </FlatButton>
+      <FlatButton onClick={() => forward(1)}>
+        <FontIcon icon="navigate_next" />
+      </FlatButton>
+      <FlatButton onClick={() => forward(10)}>
+        <FontIcon icon="fast_forward" />
+      </FlatButton>
+      <FlatButton onClick={() => forward(-1)}>
+        <FontIcon icon="skip_next" />
+      </FlatButton>
+    </GameControlButtonBar>
+  );
+}
+
+export default GameControlButtons;
