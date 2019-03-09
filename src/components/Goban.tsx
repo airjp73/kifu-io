183709bcc48c2b23033667233506aaf9d41501db
airjp73 <aaron.pettengill.violin@gmail.com>
2019-03-09 13:34:16 -0500

    Start playing around with canvas

--- src/components/Goban.tsx
+++ src/components/Goban.tsx
@@ -1,88 +1,75 @@
-import React, {
-  useState,
-  useRef,
-  useLayoutEffect,
-  useEffect,
-} from 'react';
+import React, { useState, useRef, useLayoutEffect, useEffect } from 'react';
 import styled from 'styled-components';
 import { useGoGameContext } from 'contexts/GoGameContext';
+import { panelBackground } from 'style';
 
 export type Point = 'b' | 'w' | null;
 
-const Board = styled.div`
-  background-color: brown;
-  display: grid;
-  grid-template-columns: repeat(19, 1fr);
-  grid-template-rows: repeat(19, 1fr);
-  grid-gap: 1px;
+const Board = styled.canvas`
+  background-color: ${panelBackground};
 `;
 
-// TODO: Come up with an alternative method since this doesn't work server-side
-type UseBoardHeight = () => [number, React.MutableRefObject<HTMLDivElement>];
-const useBoardHeight: UseBoardHeight = () => {
-  const [height, setHeight] = useState(0);
-  const boardRef = useRef<HTMLDivElement>(null);
-  const setSize = () => setHeight(boardRef.current.offsetWidth);
+type UseBoardWidth = () => number;
+const useBoardWidth: UseBoardWidth = (boardRef: React.Ref<HTMLCanvasElement>) => {
+  const [width, setWidth] = useState(0);
+  const setSize = () => setWidth(boardRef.current.width);
   useLayoutEffect(setSize, []);
   useEffect(() => {
     window.addEventListener('resize', setSize);
     return () => window.removeEventListener('resize', setSize);
   });
 
-  return [height, boardRef];
+  return [width, boardRef];
 };
 
-// TODO: Probably write the goban with a canvas
-interface SpaceProps {
-  invisible: boolean;
-}
-const Space = styled.div`
-  ${(props: SpaceProps) =>
-    !props.invisible ? 'background-color: tan;' : 'blackground-color: blue;'};
-  transform: translate(50%, 50%);
-`;
-
-interface PieceProps {
-  type: 'b' | 'w';
-}
-const Piece = styled.div`
-  background-color: ${(props: PieceProps) =>
-    props.type === 'b' ? 'black' : 'white'};
-  border-radius: 50%;
-  transform: translate(-50%, -50%);
-  height: 100%;
-  width: 100%;
-`;
-
-interface Space {
-  point: Point;
-  hidden: boolean;
-}
-
 const Goban = () => {
-  const [height, boardRef] = useBoardHeight();
   const { gameState } = useGoGameContext();
   const { boardState } = gameState;
+  const boardRef: React.Ref<HTMLCanvasElement> = useRef(null);
 
-  const a = 'a'.charCodeAt(0);
-  const spaces: Space[] = [];
-  for (let y = 0; y < 19; ++y) {
-    for (let x = 0; x < 19; ++x) {
-      const yChar = String.fromCharCode(y + a);
-      const xChar = String.fromCharCode(x + a);
-      const stateKey = `${xChar}${yChar}`;
-      spaces.push({ point: boardState[stateKey], hidden: x === 18 || y === 18 });
-    }
+  const stoneRadius = 10;
+  const pointWidth = 25;
+
+  const drawStone = (ctx: CanvasRenderingContext2D, x: number, y: number) => {
+    const xPos = (x + 1) * pointWidth;
+    const yPos = (y + 1) * pointWidth;
+
+    ctx.fillStyle = 'rgb(0, 0, 0)';
+    ctx.moveTo(xPos + stoneRadius, yPos);
+    ctx.arc(xPos, yPos, stoneRadius, 0, Math.PI * 2);
+  };
+
+  const drawBoard = () => {
+    // getContext function not present on unsupported browsers
+    const canvas = boardRef.current;
+    if (!canvas.getContext) return;
+
+    const ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    ctx.beginPath();
+
+    // Draw black stones
+    drawStone(ctx, 0, 0);
+    drawStone(ctx, 1, 1);
+    ctx.fill();
+
+    // Draw white stones
+    drawStone(ctx, 1, 0);
+    drawStone(ctx, 0, 1);
+    ctx.stroke();
   }
-  return (
-    <Board ref={boardRef} style={{ height }}>
-      {spaces.map((space, index) => (
-        <Space key={index} invisible={space.hidden}>
-          {space.point && <Piece type={space.point} />}
-        </Space>
-      ))}
-    </Board>
-  );
+
+  useEffect(() => {
+    drawBoard();
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener('resize', drawBoard);
+    return () => window.removeEventListener('resize', drawBoard);
+  }, []);
+
+  return <Board ref={boardRef}>Go board not supported on your browser</Board>;
 };
 
 export default Goban;
