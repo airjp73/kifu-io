402e3b7698d6d7f1b69afcffa0c2bf4ed988ec3e
airjp73 <aaron.pettengill.violin@gmail.com>
2019-03-09 23:56:29 -0500

    Hook it up to the state

--- src/components/Goban.tsx
+++ src/components/Goban.tsx
@@ -1,80 +1,70 @@
 import React, { useState, useRef, useLayoutEffect, useEffect } from 'react';
 import styled from 'styled-components';
 import { useGoGameContext } from 'contexts/GoGameContext';
-import { panelBackground } from 'style';
 
 export type Point = 'b' | 'w' | null;
 
-const Board = styled.canvas`
-  background-color: ${panelBackground};
-  width: 100%;
-`;
-
-type UseBoardWidth = () => number;
-const useBoardWidth: UseBoardWidth = (boardRef: React.Ref<HTMLCanvasElement>) => {
-  const [width, setWidth] = useState(0);
-  const setSize = () => setWidth(boardRef.current.width);
-  useLayoutEffect(setSize, []);
-  useEffect(() => {
-    window.addEventListener('resize', setSize);
-    return () => window.removeEventListener('resize', setSize);
-  });
-
-  return [width, boardRef];
-};
-
 class GobanCanvas {
-
   size: [number, number];
   canvas: HTMLCanvasElement;
 
+  private spritePadding = 2;
   private unit: number;
   private stoneRadius: number;
-
   private blackStone: HTMLCanvasElement;
   private whiteStone: HTMLCanvasElement;
 
   constructor(canvas: HTMLCanvasElement) {
     this.canvas = canvas;
-    canvas.width = canvas.clientWidth;
-    canvas.height = canvas.clientWidth;
-
+    canvas.getContext('2d').imageSmoothingEnabled = false;
     this.init();
-    this.drawBoard();
   }
 
-  init = () => {
-    this.size = [19, 19];
+  public init = () => {
+    this.calculateDimensions();
+    this.initSprites();
+    this.drawBoard();
+  };
+
+  private calculateDimensions = () => {
+    this.size = [19, 19]; // TODO: Make this dynamic
+    this.canvas.width = this.canvas.clientWidth;
+    this.canvas.height = this.canvas.clientWidth;
     this.unit = this.canvas.width / 20; // 19 points + edges
     this.stoneRadius = (this.unit - 2) / 2;
+  };
 
-    // Create re-usable stones
-    // TODO: Break out this logic into a function if adding any more stone sprites
-    this.blackStone = document.createElement('canvas');
-    this.blackStone.width = this.stoneRadius * 2;
-    this.blackStone.height = this.stoneRadius * 2;
-    const blackCtx = this.blackStone.getContext('2d');
-    blackCtx.fillStyle = '#000000';
-    blackCtx.beginPath();
-    blackCtx.moveTo(this.stoneRadius, 0);
-    blackCtx.arc(this.stoneRadius, this.stoneRadius, this.stoneRadius, 0, Math.PI * 2);
-    blackCtx.fill();
+  private initSprites = () => {
+    this.blackStone = this.createStoneSprite('#000');
+    this.whiteStone = this.createStoneSprite('#fff');
+  };
 
-    this.whiteStone = document.createElement('canvas');
-    this.whiteStone.width = this.stoneRadius * 2;
-    this.whiteStone.height = this.stoneRadius * 2;
-    const whiteCtx = this.whiteStone.getContext('2d');
-    whiteCtx.fillStyle = '#ffffff';
-    whiteCtx.beginPath();
-    whiteCtx.moveTo(this.stoneRadius, 0);
-    whiteCtx.arc(this.stoneRadius, this.stoneRadius, this.stoneRadius, 0, Math.PI * 2);
-    whiteCtx.fill();
-  }
+  private createStoneSprite = (color: string) => {
+    const canvas = document.createElement('canvas');
+    canvas.width = this.stoneRadius * 2 + this.spritePadding + 10;
+    canvas.height = this.stoneRadius * 2 + this.spritePadding + 10;
 
-  drawStone = (x: number, y: number, color: Point) => {
+    const ctx = canvas.getContext('2d');
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.moveTo(this.stoneRadius + this.spritePadding, this.spritePadding);
+    ctx.arc(
+      this.stoneRadius + this.spritePadding,
+      this.stoneRadius + this.spritePadding,
+      this.stoneRadius,
+      0,
+      Math.PI * 2
+    );
+    ctx.fill();
+    return canvas;
+  };
+
+  public drawStone = (x: number, y: number, color: Point) => {
     const ctx = this.canvas.getContext('2d');
-    const xCoord = this.getCoord(x) - this.unit / 2 + 1;
-    const yCoord = this.getCoord(y) - this.unit / 2 + 1;
+
+    // We want the center of the stone sprite on the point, so subtract the radius and sprite padding
+    const xCoord = this.getCoord(x) - this.stoneRadius - this.spritePadding;
+    const yCoord = this.getCoord(y) - this.stoneRadius - this.spritePadding;
     const stone = color === 'b' ? this.blackStone : this.whiteStone;
     ctx.drawImage(stone, xCoord, yCoord);
   };
@@ -107,13 +97,16 @@ class GobanCanvas {
     }
 
     // Draw
-    ctx.stroke()
+    ctx.stroke();
     ctx.closePath();
-  }
+  };
 
-  getCoord = (coord: number) => (coord * this.unit) + this.unit;
-
+  private getCoord = (coord: number) => coord * this.unit + this.unit;
 }
+
+const Board = styled.canvas`
+  width: 100%;
+`;
 
 const Goban = () => {
   const { gameState } = useGoGameContext();
@@ -121,29 +114,29 @@ const Goban = () => {
   const boardRef: React.Ref<HTMLCanvasElement> = useRef(null);
   const goban: React.MutableRefObject<GobanCanvas> = useRef(null);
 
-  const stoneRadius = 10;
-  const pointWidth = 25;
+  const drawBoardState = () => {
+    if (!goban.current) goban.current = new GobanCanvas(boardRef.current);
+    else goban.current.drawBoard();
 
-  const drawBoard = () => {
-    // getContext function not present on unsupported browsers
-    const canvas = boardRef.current;
-    if (!canvas.getContext) return;
+    Object.entries(boardState).forEach(([point, color]) => {
+      const A = 'a'.charCodeAt(0);
+      const x = point.charCodeAt(0) - A;
+      const y = point.charCodeAt(1) - A;
+      goban.current.drawStone(x, y, color);
+    });
+  };
 
-    const ctx = canvas.getContext('2d');
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-  }
+  useEffect(() => drawBoardState(), [boardState]);
+
+  const handleResize = () => {
+    goban.current.init();
+    drawBoardState();
+  };
 
   useEffect(() => {
-    goban.current = new GobanCanvas(boardRef.current);
-
-    goban.current.drawStone(4, 4, 'b');
-    goban.current.drawStone(5, 4, 'w');
-  }, []);
-
-  // useEffect(() => {
-  //   window.addEventListener('resize', drawBoard);
-  //   return () => window.removeEventListener('resize', drawBoard);
-  // }, []);
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  });
 
   return <Board ref={boardRef}>Go board not supported on your browser</Board>;
 };
