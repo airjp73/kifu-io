2997a09f5763ad372d8aad11b417ec740a5bce24
airjp73 <aaron.pettengill.violin@gmail.com>
2019-03-13 20:39:11 -0400

    Add markup test sgf and use it

---
+++ src/parseSgf/snapshots/snapshot5.ts
@@ -0,0 +1,9 @@
+export default `
+(;GM[1]FF[4]CA[UTF-8]AP[CGoban:3]ST[2]
+RU[Japanese]SZ[19]KM[0.00]
+PW[White]PB[Black]
+;B[pd]
+;W[dd]
+;B[pp]
+;W[dp]CR[ih][jh][kh]LB[ii:A][ji:B][ki:C][ij:1][jj:2][kj:3]TR[if][jf][kf]SQ[ig][jg][kg])
+`;
