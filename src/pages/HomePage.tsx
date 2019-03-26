65f1a19c624200d784f0b6e15363aaa68d816b7f
airjp73 <aaron.pettengill.violin@gmail.com>
2019-03-26 12:13:44 -0400

    Add variation heavy snapshot file

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,7 +4,7 @@ import Layout from 'components/Layout';
 import Goban from 'components/Goban';
 import { GoGameContextProvider } from 'contexts/GoGameContext';
 import GameControlButtons from 'components/GameControlButtons';
-import sgf from 'parseSgf/snapshots/snapshot1';
+import sgf from 'parseSgf/snapshots/snapshot6';
 import GameInfo from 'components/GameInfo';
 
 const GameView = styled.div`
