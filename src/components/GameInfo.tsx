153f29c42b61924478d2420849b540253f2606c8
airjp73 <aaron.pettengill.violin@gmail.com>
2019-03-31 00:23:29 -0400

    Try some stuff

--- src/components/GameInfo.tsx
+++ src/components/GameInfo.tsx
@@ -1,9 +1,35 @@
 import React from 'react';
 import GameTreeView from 'components/GameTreeView';
 import GameComments from './GameComments';
-import Tabs from './Tabs/Tabs';
+import Tabs, { useTabContext } from './Tabs/Tabs';
 import TabBar from './Tabs/TabBar';
 import TabContent from './Tabs/TabContent';
+import posed, { PoseGroup } from 'react-pose';
+
+const TabContentArea = posed.div({
+  enter: {
+    x: 0,
+    delay: 300,
+    beforeChildren: true,
+  },
+  exit: {
+    x: 50,
+  },
+});
+
+const TabBody: React.FunctionComponent = ({ children }) => {
+  const { currentTab } = useTabContext();
+  return (
+    <PoseGroup>
+      {React.Children.toArray(children)
+        .filter(child => child && child.type === TabContent)
+        .map(child => (
+          <TabContentArea css={`height:100%;`} key={child.props.tab}>{child}</TabContentArea>
+        ))}
+      {/* <TabContentArea key={currentTab}>{children}</TabContentArea> */}
+    </PoseGroup>
+  );
+};
 
 // TODO: Add tabs to this component to switch between different views
 const GameInfo = () => (
@@ -15,12 +41,14 @@ const GameInfo = () => (
         { value: 'more-info', icon: 'more_vert' },
       ]}
     />
-    <TabContent tab="comments">
-      <GameComments />
-    </TabContent>
-    <TabContent tab="game-tree">
-      <GameTreeView />
-    </TabContent>
+    <TabBody>
+      <TabContent tab="comments">
+        <GameComments />
+      </TabContent>
+      <TabContent tab="game-tree">
+        <GameTreeView />
+      </TabContent>
+    </TabBody>
   </Tabs>
 );
 
--- src/components/Tabs/TabContent.tsx
+++ src/components/Tabs/TabContent.tsx
@@ -1,10 +1,19 @@
 import React, { ReactElement } from 'react';
 import styled from 'styled-components';
 import { useTabContext } from './Tabs';
+import posed from 'react-pose';
 
 const TabContentArea = styled.div`
   flex: 1;
 `;
+// const TabContentArea = posed(TabContentStyle)({
+//   enter: {
+//     x: 0,
+//   },
+//   exit: {
+//     x: 50,
+//   },
+// });
 
 interface TabContentProps {
   tab: string;
@@ -15,7 +24,9 @@ const TabContent: React.FunctionComponent<TabContentProps> = ({
   tab,
 }) => {
   const { currentTab } = useTabContext();
-  return currentTab === tab && <TabContentArea>{children}</TabContentArea>;
+  return (
+    currentTab === tab && <TabContentArea key={tab}>{children}</TabContentArea>
+  );
 };
 
 export default TabContent;
