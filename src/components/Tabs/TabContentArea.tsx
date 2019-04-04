9f6ea49c00a94a63c11e3203c7454af8ee92aa30
airjp73 <aaron.pettengill.violin@gmail.com>
2019-04-04 17:35:11 -0400

    Redo tab animations with spring

--- src/components/Tabs/TabContentArea.tsx
+++ src/components/Tabs/TabContentArea.tsx
@@ -1,27 +1,20 @@
-import React, { useEffect, useMemo, useRef } from 'react';
-import posed, { PoseGroup } from 'react-pose';
+import React, { useMemo } from 'react';
+import { useTransition, animated } from 'react-spring';
 import { useTabContext } from './Tabs';
 import usePrevious from 'hooks/usePrevious';
+import styled from 'styled-components';
 
-const transition = { duration: 250, ease: 'easeInOut' };
-interface TabContentContainerProps {
-  direction: 'left' | 'right';
-}
-const TabContentContainer = posed.div({
-  preEnter: {
-    x: ({ direction }: TabContentContainerProps) =>
-      direction === 'left' ? '-100%' : '100%',
-  },
-  enter: {
-    x: 0,
-    transition,
-  },
-  exit: {
-    x: ({ direction }: TabContentContainerProps) =>
-      direction === 'left' ? '100%' : '-100%',
-    transition,
-  },
-});
+const ParentTabContainer = styled.div`
+  position: relative;
+  height: 100%;
+`;
+const TabContentContainer = animated(styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 0;
+`);
 
 const TabContentArea: React.FunctionComponent = ({ children }) => {
   const { currentTab, tabs } = useTabContext();
@@ -33,23 +26,38 @@ const TabContentArea: React.FunctionComponent = ({ children }) => {
     return prevIndex < currentIndex ? 'right' : 'left';
   }, [currentTab]);
 
+  const transitions = useTransition(currentTab, null, {
+    from: {
+      transform:
+        animDirection === 'left' ? 'translateX(-100%)' : 'translateX(100%)',
+      opacity: 0,
+    },
+    enter: {
+      transform: 'translateX(0)',
+      opacity: 1,
+      flex: 1,
+    },
+    leave: {
+      transform:
+        animDirection === 'left' ? 'translateX(100%)' : 'translateX(-100%)',
+      opacity: 0,
+    },
+  });
+
   return (
-    <PoseGroup preEnterPose="preEnter" direction={animDirection}>
-      <TabContentContainer
-        css={`
-          flex: 1;
-        `}
-        key={currentTab}
-      >
-        {/*
+    <ParentTabContainer>
+      {transitions.map(({ item, props, key }) => (
+        <TabContentContainer key={key} style={props}>
+          {/*
           Pass in props directly instead of tapping into the context
           So that the old container that's animating out doesn't update
         */}
-        {React.Children.map(children, (child: React.ReactElement) =>
-          React.cloneElement(child, { currentTab })
-        )}
-      </TabContentContainer>
-    </PoseGroup>
+          {React.Children.map(children, (child: React.ReactElement) =>
+            React.cloneElement(child, { currentTab: item })
+          )}
+        </TabContentContainer>
+      ))}
+    </ParentTabContainer>
   );
 };
 
