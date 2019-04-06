9f6339b53eafc9e7b059dc2019fa285cfec7e3db
airjp73 <aaron.pettengill.violin@gmail.com>
2019-04-06 08:03:32 -0400

    Use spring instead of pose

--- src/components/SlideOutPanel.tsx
+++ src/components/SlideOutPanel.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import posed, { PoseGroup } from 'react-pose';
+import { animated, config, useTransition } from 'react-spring';
 import { highlight, panelBackground } from 'style';
 import Portal from './Portal';
 
@@ -14,12 +14,6 @@ interface PanelProps {
   side: string;
 }
 
-const PanelPose = posed.div({
-  closed: {
-    x: (props: PanelProps) => (props.side === 'left' ? '-20rem' : '20rem'),
-  },
-  open: { x: '0rem' },
-});
 const panelWidth = '20rem';
 const rightPanel = `
   right: 0;
@@ -29,7 +23,7 @@ const leftPanel = `
   left: 0;
   transform: translateX(-${panelWidth});
 `;
-const Panel = styled(PanelPose)`
+const Panel = animated(styled.div`
   position: fixed;
   height: 100vh;
   background-color: ${panelBackground};
@@ -38,58 +32,67 @@ const Panel = styled(PanelPose)`
   width: ${panelWidth};
 
   ${(props: PanelProps) => (props.side === 'right' ? rightPanel : leftPanel)}
-`;
-
-const OverlayPose = posed.div({
-  closed: { opacity: 0 },
-  open: {
-    opacity: 1,
-    transition: {
-      duration: 200,
-      ease: 'linear',
-    },
-  },
-});
-const Overlay = styled(OverlayPose)`
+`);
+const Overlay = animated(styled.div`
   position: fixed;
   height: 100vh;
   width: 100vw;
   background-color: rgba(0, 0, 0, 0.5);
-`;
+`);
 
 const SlideOutPanel: React.FunctionComponent<SidePanelProps> = ({
   active,
   children,
   onClose,
   side = 'left',
-}) => (
-  <Portal>
-    <PoseGroup
-      preEnterPose="closed"
-      enterPose="open"
-      exitPose="closed"
-      css={`
-        position: absolute;
-        right: 0;
-        top: 0;
-        left: 0;
-        bottom: 0;
-      `}
-    >
-      {active && (
-        <Overlay
-          key="overlay"
-          onClick={onClose}
-          data-testid="slide-out-panel-overlay"
-        />
+}) => {
+  const transition = useTransition(active, null, {
+    from: {
+      opacity: 0,
+      transform: side === 'left' ? 'translateX(-100%)' : 'translateX(100%)',
+    },
+    enter: { opacity: 1, transform: 'translateX(0);' },
+    leave: {
+      opacity: 0,
+      transform: side === 'left' ? 'translateX(-100%)' : 'translateX(100%)',
+    },
+    config: { ...config.default, velocity: 10 },
+  });
+
+  return (
+    <Portal>
+      {transition.map(
+        ({ item, key, props }) =>
+          item && (
+            <div
+              css={`
+                position: absolute;
+                right: 0;
+                top: 0;
+                left: 0;
+                bottom: 0;
+              `}
+              key={key}
+            >
+              <Overlay
+                key="overlay"
+                onClick={onClose}
+                data-testid="slide-out-panel-overlay"
+                style={{ opacity: props.opacity }}
+              />
+              <Panel
+                key="panel"
+                side={side}
+                style={{ transform: props.transform }}
+                data-testid="slide-out-panel"
+              >
+                {children}
+              </Panel>
+            </div>
+          )
       )}
-      {active && (
-        <Panel key="panel" side={side} data-testid="slide-out-panel">
-          {children}
-        </Panel>
-      )}
-    </PoseGroup>
-  </Portal>
-);
+    </Portal>
+  );
+};
 
 export default SlideOutPanel;
