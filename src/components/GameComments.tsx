a96ae53fa6b69f258e22aa05fa5e0a40242100d8
airjp73 <aaron.pettengill.violin@gmail.com>
2019-04-06 18:27:20 -0400

    Add features to game comments

--- src/components/GameComments.tsx
+++ src/components/GameComments.tsx
@@ -1,28 +1,59 @@
 import React from 'react';
-import { useGoGameContext } from 'contexts/GoGameContext';
 import styled from 'styled-components';
+import { useGoGameContext } from 'contexts/GoGameContext';
+import { PositionStatus } from 'contexts/gameStateReducer';
 
+const Comment = styled.p`
+  white-space: pre-wrap;
+  margin: 1rem 0;
+`;
+const EmptyText = styled.p`
+  margin-top: 1rem;
+  opacity: 0.5;
+  font-style: italic;
+`;
 const GameCommentContainer = styled.div`
-  padding: 1rem;
+  padding: 0 1rem;
 
-  h4 {
-    margin-top: 0;
+  h3 {
+    margin-bottom: 1rem;
   }
   pre {
     white-space: pre-wrap;
+    margin: 1rem 0;
   }
   em {
-    opacity: .5;
+    font-weight: 600;
   }
 `;
 
+const getStatusMessage = ({ favoredPlayer, magnitude }: PositionStatus) => {
+  switch (favoredPlayer) {
+    case 'b':
+      return magnitude === 1 ? 'Good for black' : 'Very good for black';
+    case 'w':
+      return magnitude === 1 ? 'Good for white' : 'Very good for white';
+    case 'even':
+      return magnitude === 1 ? 'Position is event' : 'Joseki';
+    case 'unclear':
+      return magnitude === 1
+        ? 'Position is unclear'
+        : 'Position is very unclear';
+    default:
+      return null;
+  }
+};
+
 const GameComments = () => {
   const { gameState } = useGoGameContext();
-  const { name, comment } = gameState.moveState;
+  const { name, comment, positionStatus } = gameState.moveState;
+  const showEmptyMessage = !(name || comment || positionStatus);
   return (
     <GameCommentContainer>
-      {name && <h4>{name}</h4>}
-      {comment ? <pre>{comment}</pre> : <em>No comments on this move</em>}
+      {name && <h3>{name}</h3>}
+      {positionStatus && <em>{getStatusMessage(positionStatus)}</em>}
+      {comment && <Comment>{comment}</Comment>}
+      {showEmptyMessage && <EmptyText>No comments on this move</EmptyText>}
     </GameCommentContainer>
   );
 };
