a9bedb44f26d7b0c03cbcef1b40eb7415876cd20
airjp73 <aaron.pettengill.violin@gmail.com>
2019-04-11 20:38:20 -0400

    Go pretty-nuclear

--- src/parseSgf/snapshots/index.ts
+++ src/parseSgf/snapshots/index.ts
@@ -1,8 +1,4 @@
 import snapshot1 from './snapshot1';
 import snapshot2 from './snapshot2';
 import snapshot3 from './snapshot3';
-export default [
-  snapshot1,
-  snapshot2,
-  snapshot3
-];
+export default [snapshot1, snapshot2, snapshot3];
