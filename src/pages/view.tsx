935365e7ee25499af57be5b2b12ca2105c0e79aa
airjp73 <aaron.pettengill.violin@gmail.com>
2019-05-19 15:43:33 -0400

    Remove unused import

--- src/pages/view.tsx
+++ src/pages/view.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import GameView from 'goban/GameView';
 import Layout from 'components/Layout';
-import Spinner from 'components/Spinner';
 
 const GameViewPage: React.FunctionComponent = () => (
   <Layout>
