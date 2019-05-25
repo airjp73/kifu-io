a666582e0fea19f9f14a28dbe1b24d336807b121
airjp73 <aaron.pettengill.violin@gmail.com>
2019-05-25 16:46:01 -0400

    Move the button so it looks okay on mobile too

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -5,10 +5,10 @@ import LogoutButton from 'components/LogoutButton';
 const Profile = () => {
   const [currentUser] = useCurrentUser();
   return (
-    <div style={{ width: '100%', position: 'relative' }}>
+    <div style={{ width: '100%' }}>
       <h1>Welcome {currentUser.displayName}</h1>
       <p>Coming soon: A list of all your uploaded sgfs</p>
-      <LogoutButton style={{ position: 'absolute', top: 0, right: 0 }} />
+      <LogoutButton style={{ position: 'absolute', bottom: 0, right: 0 }} />
     </div>
   );
 };
