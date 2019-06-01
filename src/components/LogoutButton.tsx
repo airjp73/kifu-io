3a84a29d94977f557d1d975e70d4d74dfb1cbfcb
airjp73 <aaron.pettengill.violin@gmail.com>
2019-06-01 11:07:01 -0400

    Redirect to login on log out

--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
+import { withRouter, RouteComponentProps } from 'react-router-dom';
 import firebase from 'api/firebase';
 import FlatButton from './FlatButton';
 
 const auth = firebase.auth();
 
 const LogoutButton: React.FunctionComponent<
-  React.ComponentProps<typeof FlatButton>
-> = props => (
-  <FlatButton {...props} onClick={() => auth.signOut()}>
+  React.ComponentProps<typeof FlatButton> & Partial<RouteComponentProps>
+> = ({ history, ...rest }) => (
+  <FlatButton
+    {...rest}
+    onClick={async () => {
+      await auth.signOut();
+      history.push('/login');
+    }}
+  >
     Log Out
   </FlatButton>
 );
 
-export default LogoutButton;
+export default withRouter(LogoutButton);
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'styled-components/macro';
 import useCurrentUser from 'hooks/useCurrentUser';
 import LogoutButton from 'components/LogoutButton';
 
@@ -8,7 +9,13 @@ const Profile = () => {
     <div style={{ width: '100%' }}>
       <h1>Welcome {currentUser.displayName}</h1>
       <p>Coming soon: A list of all your uploaded sgfs</p>
-      <LogoutButton style={{ position: 'absolute', bottom: '1rem', right: '1rem' }} />
+      <LogoutButton
+        css={`
+          position: 'absolute';
+          bottom: '1rem';
+          right: '1rem';
+        `}
+      />
     </div>
   );
 };
