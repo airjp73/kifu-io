0085441529936edb8fcfb51074e2524a163d8a42
airjp73 <aaron.pettengill.violin@gmail.com>
2019-06-02 12:02:26 -0400

    Fix snapshot test

--- src/goban/__test__/GoGameContext.test.tsx
+++ src/goban/__test__/GoGameContext.test.tsx
@@ -4,6 +4,7 @@ import { render, fireEvent } from 'react-testing-library';
 import sgf1 from 'goban/parseSgf/snapshots/snapshot1';
 import sgf2 from 'goban/parseSgf/snapshots/snapshot2';
 import sgf3 from 'goban/parseSgf/snapshots/snapshot3';
+import useSgf from '../useSgf';
 import { GoGameContextProvider, useGoGameContext } from '../GoGameContext';
 import { createStringFromBoardState } from '../boardStateTestHelpers';
 
@@ -24,11 +25,14 @@ const BoardStateTestRenderer = () => {
 interface TestGoGameProps {
   sgf: string;
 }
-const TestGoGame: React.FunctionComponent<TestGoGameProps> = ({ sgf }) => (
-  <GoGameContextProvider sgf={sgf}>
-    <BoardStateTestRenderer />
-  </GoGameContextProvider>
-);
+const TestGoGame: React.FunctionComponent<TestGoGameProps> = ({ sgf }) => {
+  const [gameTree] = useSgf(sgf);
+  return (
+    <GoGameContextProvider gameTree={gameTree}>
+      <BoardStateTestRenderer />
+    </GoGameContextProvider>
+  );
+};
 
 describe('Game context snapshots', () => {
   each([['sgf1', sgf1], ['sgf2', sgf2], ['sgf3', sgf3]]).test(
