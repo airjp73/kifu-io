1a9263b925a535b50a5cb5b2caed9dabfa36b6f3
airjp73 <aaron.pettengill.violin@gmail.com>
2019-06-02 11:02:36 -0400

    Start styling the form

--- src/forms/UploadSgfForm.tsx
+++ src/forms/UploadSgfForm.tsx
@@ -1,8 +1,12 @@
 import React, { useEffect, useState, useRef } from 'react';
+import styled from 'styled-components';
+import 'styled-components/macro';
 import { UploadInput } from 'components/Input';
 import { GoGameContextProvider } from 'goban/GoGameContext';
+import SimpleContent from 'components/SimpleContent';
 import Goban from 'goban/Goban';
 import GameControlButtons from 'goban/GameControlButtons';
+import Button from 'components/Button';
 
 const useFileContents = (file?: File): null | string => {
   const [contents, setContents] = useState<string>(null);
@@ -25,6 +29,26 @@ const useFileContents = (file?: File): null | string => {
   return contents;
 };
 
+const UploadPreview = styled.div`
+  grid-area: preview;
+`;
+const UploadFormFields = styled(SimpleContent)`
+  grid-area: fields;
+`;
+const UploadControlButtons = styled(GameControlButtons)`
+  grid-area: buttons;
+`;
+
+const UploadForm = styled.form`
+  height: 100%;
+  display: grid;
+  grid-template-areas:
+    'fields preview'
+    'fields buttons';
+  grid-template-columns: auto 1fr;
+  grid-template-rows: 1fr auto;
+`;
+
 const UploadSgfForm = () => {
   const [file, setFile] = useState<File>(null);
   const contents = useFileContents(file);
@@ -33,17 +57,38 @@ const UploadSgfForm = () => {
     setFile(event.currentTarget.files[0]);
 
   return (
-    <>
-      <UploadInput label="SGF File" onChange={handleChange} />
+    <UploadForm
+      onSubmit={e => {
+        e.preventDefault();
+        console.log('Submitted!');
+      }}
+    >
+      <UploadFormFields>
+        <h2>Choose a file to upload</h2>
+        <UploadInput label="SGF File" onChange={handleChange} />
+        <Button
+          css={`
+            margin-left: auto;
+          `}
+          type="submit"
+          icon="cloud_upload"
+        >
+          Upload
+        </Button>
+      </UploadFormFields>
       {contents && (
         <GoGameContextProvider key={contents} sgf={contents}>
-          <div style={{ display: 'grid', height: '500px' }}>
-            <Goban />
-          </div>
-          <GameControlButtons />
+          <UploadPreview>
+            <Goban
+              css={`
+                height: 100%;
+              `}
+            />
+          </UploadPreview>
+          <UploadControlButtons />
         </GoGameContextProvider>
       )}
-    </>
+    </UploadForm>
   );
 };
 
