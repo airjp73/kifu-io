2a72ccac5e9bfe1777c8b576906e7b84f03d01cb
airjp73 <aaron.pettengill.violin@gmail.com>
2019-06-23 20:44:26 -0400

    Upload an sgf and push to the profile page when done

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { RouteComponentProps } from 'react-router-dom';
 import useCurrentUser from 'hooks/useCurrentUser';
 import LogoutButton from 'components/LogoutButton';
 
-const Profile = () => {
+const Profile: React.FunctionComponent<Partial<RouteComponentProps>> = ({
+  location,
+}) => {
   const [currentUser] = useCurrentUser();
   return (
     <div style={{ width: '100%' }}>
       <h1>Welcome {currentUser.displayName}</h1>
+      {location.state.uploadSucceeded && <p>Upload Succeeded!</p>}
       <p>Coming soon: A list of all your uploaded sgfs</p>
       <LogoutButton
         style={{
