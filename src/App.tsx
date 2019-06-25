ce8765485cb3eda00a80b8dc2f529a1bcd074dfb
airjp73 <aaron.pettengill.violin@gmail.com>
2019-06-25 07:09:59 -0400

    Add simple home page

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,9 @@ import Profile from 'pages/Profile';
 import Upload from 'pages/Upload';
 import AuthRoute from 'components/AuthRoute';
 import Layout from 'components/Layout';
-import AppContext from './AppContext';
 import 'normalize.css';
+import AppContext from './AppContext';
+import Home from 'pages/Home';
 
 const GlobalStyles = createGlobalStyle`
   html {
@@ -38,7 +39,7 @@ const App = () => (
     <GlobalStyles />
     <AppContext>
       <Layout>
-        <Route exact path="/" render={() => <Redirect to="/view/sample" />} />
+        <Route exact path="/" component={Home} />
         <Route path="/login" component={Login} />
         <Route path="/view/:gameId" component={View} />
         <AuthRoute path="/profile" component={Profile} />
