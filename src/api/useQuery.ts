4d8fd07ee2657133c0c6c9e9984bc6d8f0ca3c2d
airjp73 <aaron.pettengill.violin@gmail.com>
2019-06-27 21:59:20 -0400

    Add pagination support to useQuery

--- src/api/useQuery.ts
+++ src/api/useQuery.ts
@@ -1,24 +1,62 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback, useMemo } from 'react';
 import { FirebaseEntity } from './apiDataTypes';
+
+interface QueryActions {
+  nextPage: () => void;
+  prevPage: () => void;
+}
 
 const useQuery = <T extends FirebaseEntity>(
   query: firebase.firestore.Query
-): [T[], boolean] => {
-  const [data, setData] = useState(null);
+): [T[], boolean, QueryActions] => {
+  const [result, setResult] = useState<firebase.firestore.QuerySnapshot>(null);
   const [loading, setLoading] = useState(true);
+  const docs = useMemo(
+    () =>
+      result
+        ? (result.docs.map(doc => ({ id: doc.id, ...doc.data() })) as T[])
+        : [],
+    [result]
+  );
+
+  const fetchData = useCallback(
+    async (
+      startAfter?: firebase.firestore.QueryDocumentSnapshot,
+      endBefore?: firebase.firestore.QueryDocumentSnapshot
+    ) => {
+      setLoading(true);
+
+      let fullQuery = startAfter ? query.startAfter(startAfter) : query;
+      fullQuery = endBefore ? query.endBefore(endBefore) : fullQuery;
+      const result = await fullQuery.get();
+      setResult(result);
+
+      setLoading(false);
+    },
+    [query, setResult, setLoading]
+  );
 
   useEffect(() => {
-    async function fetchData() {
-      const result = await query.get();
-      const docs = result.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-      setData(docs);
-      setLoading(false);
-    }
-
     fetchData();
-  }, [query, setData, setLoading]);
+  }, [query, setResult, setLoading, fetchData]);
 
-  return [data, loading];
+  const nextPage = useCallback(() => {
+    fetchData(result.docs[result.docs.length]);
+  }, [fetchData, result]);
+
+  const prevPage = useCallback(() => {
+    fetchData(result.docs[0]);
+  }, [fetchData, result]);
+
+  const actions: QueryActions = useMemo(
+    () => ({
+      nextPage,
+      prevPage,
+    }),
+    [nextPage, prevPage]
+  );
+
+  return [docs, loading, actions];
 };
 
 export default useQuery;
