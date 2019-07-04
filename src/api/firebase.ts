3cb2e88944f7b004d53f268e12bea82ede6a0182
airjp73 <aaron.pettengill.violin@gmail.com>
2019-07-04 12:02:09 -0400

    Create a production config object

--- src/api/firebase.ts
+++ src/api/firebase.ts
@@ -2,9 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
 
-// DEVELOPMENT ONLY
-// TODO: Use env variables to configure separate production environment
-const config = {
+const devConfig = {
   apiKey: 'AIzaSyAFRHkIeSpcffE8sH3EoX-4oEkZP5c7U1o',
   authDomain: 'go-reviews.firebaseapp.com',
   databaseURL: 'https://go-reviews.firebaseio.com',
@@ -12,6 +10,18 @@ const config = {
   storageBucket: 'go-reviews.appspot.com',
   messagingSenderId: '548448055248',
 };
+
+const prodConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+};
+
+const config = process.env.NODE_ENV === 'production' ? prodConfig : devConfig;
 
 // Check that we haven't already initialized
 // so hot-reloading doesn't cause an error
