87652a1e5391c0c1675bf41dee6ad20e3802a8dc
airjp73 <aaron.pettengill.violin@gmail.com>
2019-07-13 12:37:20 -0400

    Delete smoke test

--- src/components/__test__/NavMenu.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import renderWithContext from 'util/renderWithContext';
-import NavMenu from '../NavMenu';
-
-test('should render', () => {
-  const rendered = renderWithContext(<NavMenu />);
-  expect(rendered).toBeTruthy();
-});
