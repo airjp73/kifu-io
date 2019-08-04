db28a661a47a5f16156696037c6abce0e4bc2d50
airjp73 <aaron.pettengill.violin@gmail.com>
2019-08-04 19:12:38 -0400

    Fill out a goban layer

---
+++ src/goban/StoneLayer.tsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useRef, useMemo, useCallback } from 'react';
+import CanvasLayer from './canvas/CanvasLayer';
+import useGobanLayer from './useGobanLayer';
+import { useGoGameContext } from './GoGameContext';
+import {
+  createBlackStone,
+  createWhiteStone,
+  calculateStonePadding,
+} from 'canvas/createStoneSprite';
+import { StoneColor } from './Goban';
+
+const pointToXY = (point: string): [number, number] => {
+  const A = 'a'.charCodeAt(0);
+  const x = point.charCodeAt(0) - A;
+  const y = point.charCodeAt(1) - A;
+  return [x, y];
+};
+
+const StoneLayer: React.FC = () => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const { height, width, stoneRadius, getCoord } = useGobanLayer();
+
+  const { gameState } = useGoGameContext();
+  const { boardState } = gameState;
+
+  const blackStone = useMemo(() => createBlackStone(stoneRadius), [
+    stoneRadius,
+  ]);
+  const whiteStone = useMemo(() => createWhiteStone(stoneRadius), [
+    stoneRadius,
+  ]);
+  const stoneSize = useMemo(() => {
+    const width = blackStone.style.width;
+    return parseInt(width.substr(0, width.length - 2));
+  }, [blackStone]);
+
+  const drawStone = useCallback(
+    (x: number, y: number, color: StoneColor) => {
+      const stone = color === 'b' ? blackStone : whiteStone;
+
+      // We want the center of the sprite on the point, so subtract the radius and sprite padding
+      const ctx = canvasRef.current.getContext('2d');
+      const stonePadding = calculateStonePadding(stoneRadius);
+      const xCoord = Math.floor(getCoord(x) - stoneRadius - stonePadding + 0.5);
+      const yCoord = Math.floor(getCoord(y) - stoneRadius - stonePadding + 0.5);
+      ctx.drawImage(stone, xCoord, yCoord, stoneSize, stoneSize);
+    },
+    [blackStone, whiteStone, getCoord, stoneSize]
+  );
+
+  useEffect(() => {
+    Object.entries(boardState).forEach(([point, color]) => {
+      const [x, y] = pointToXY(point);
+      drawStone(x, y, color);
+    });
+  }, [boardState, drawStone]);
+
+  return <CanvasLayer ref={canvasRef} height={height} width={width} />;
+};
+
+export default StoneLayer;
---
+++ src/goban/useGobanLayer.ts
@@ -0,0 +1,54 @@
+import { useContext, useMemo } from 'react';
+import CanvasContext from './canvas/CanvasContext';
+import { useGoGameContext } from './GoGameContext';
+
+interface GobanLayerData {
+  height: number;
+  width: number;
+  unit: number;
+  stoneRadius: number;
+  getCoord: (coord: number) => number;
+}
+
+/**
+ * Given the size provided by the current canvas context and the size of the goban,
+ * calculate the dimensions, unit length, and stone radius.
+ */
+const useGobanLayer = (): GobanLayerData => {
+  const { height, width } = useContext(CanvasContext);
+  const goGameContext = useGoGameContext();
+  const boardSize = goGameContext.gameState.properties.boardSize || [19, 19];
+
+  return useMemo(() => {
+    const unit = calculateUnit(height, width, boardSize);
+    const stoneRadius = unit / 2.08;
+
+    return {
+      height: unit * boardSize[1] + 1,
+      width: unit * boardSize[0] + 1,
+      unit,
+      stoneRadius,
+      getCoord: coord => coord * unit + unit - 0.5,
+    };
+  }, [height, width, boardSize[0], boardSize[1]]);
+};
+
+/**
+ * Since the board can technically be rectangular,
+ * we need to calculate the unit based on the longest side.
+ *
+ * @param height The height of the canvas container
+ * @param width The width of the canvas container
+ * @param boardSize The dimensions of the go board
+ */
+function calculateUnit(
+  height: number,
+  width: number,
+  boardSize: [number, number]
+) {
+  const widthUnit = width / (boardSize[0] + 1);
+  const heightUnit = height / (boardSize[1] + 1);
+  return Math.min(widthUnit, heightUnit);
+}
+
+export default useGobanLayer;
