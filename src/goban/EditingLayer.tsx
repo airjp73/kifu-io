3ebe320c5f29b12f5d88eb895cfb4f43f405ec75
airjp73 <aaron.pettengill.violin@gmail.com>
2019-08-10 11:05:39 -0400

    Track mouse position

--- src/goban/EditingLayer.tsx
+++ src/goban/EditingLayer.tsx
@@ -1,14 +1,59 @@
-import React, { useRef } from 'react';
+import React, { useState, useMemo, useRef } from 'react';
 import CanvasLayer from './canvas/CanvasLayer';
 import useGobanLayer from './useGobanLayer';
 import { useGoGameContext } from './GoGameContext';
 
-const EditingLayer = () => {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-  const { height, width, getCoord, unit, stoneRadius } = useGobanLayer();
-  const context = useGoGameContext();
+interface EditingLayerProps {
+  blackStoneFactory: (stoneRadius: number) => HTMLCanvasElement;
+  whiteStoneFactory: (stoneRadius: number) => HTMLCanvasElement;
+}
 
-  return <CanvasLayer ref={canvasRef} height={height} width={width} />;
+interface MouseCoords {
+  x: number;
+  y: number;
+}
+
+const EditingLayer: React.FC<EditingLayerProps> = ({
+  blackStoneFactory,
+  whiteStoneFactory,
+}) => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const { height, width, coordToPointIndex, stoneRadius } = useGobanLayer();
+  const context = useGoGameContext();
+  const [mouseCoords, setMouseCoords] = useState<MouseCoords | null>(null);
+
+  const blackStone = useMemo(() => blackStoneFactory(stoneRadius), [
+    stoneRadius,
+    blackStoneFactory,
+  ]);
+  const whiteStone = useMemo(() => whiteStoneFactory(stoneRadius), [
+    stoneRadius,
+    whiteStoneFactory,
+  ]);
+
+  const handleMouseMove = (
+    event: React.MouseEvent<HTMLCanvasElement, MouseEvent>
+  ) => {
+    const x = coordToPointIndex(event.nativeEvent.offsetX);
+    const y = coordToPointIndex(event.nativeEvent.offsetY);
+    if (!mouseCoords || x !== mouseCoords.x || y !== mouseCoords.y) {
+      setMouseCoords({ x, y });
+    }
+  };
+
+  const handleMouseLeave = () => setMouseCoords(null);
+
+  console.log(mouseCoords);
+
+  return (
+    <CanvasLayer
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseLeave}
+      ref={canvasRef}
+      height={height}
+      width={width}
+    />
+  );
 };
 
 export default EditingLayer;
