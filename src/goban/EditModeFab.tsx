b3b2dfd325d3f7df7818a5dc1e1e209a5dd3cef4
airjp73 <aaron.pettengill.violin@gmail.com>
2020-02-02 16:40:57 -0500

    Save sgf with changes and push to changed version

--- src/goban/EditModeFab.tsx
+++ src/goban/EditModeFab.tsx
@@ -3,21 +3,40 @@ import { Edit, Save } from 'react-feather';
 import Fab from 'components/Fab';
 import { useGoGameContext } from './GoGameContext';
 import { startEditing } from './actions';
-import { Prompt } from 'react-router-dom';
+import { Prompt, useHistory } from 'react-router-dom';
+import { Location } from 'history';
+import createSgfFromGameTree from './parseSgf/createSgfFromGameTree';
+import uploadSgf from 'forms/uploadSgf';
+import useCurrentUser from 'hooks/useCurrentUser';
+
+type LocationState = { afterSave: boolean };
 
 const EditModeFab: React.FC = () => {
   const {
-    gameState: { editMode },
+    gameState: { editMode, gameTree },
     dispatch,
   } = useGoGameContext();
+  const history = useHistory<LocationState>();
+  const [currentUser] = useCurrentUser();
+
+  const handleEdit = () => dispatch(startEditing());
+
+  const handleSave = async () => {
+    const sgf = createSgfFromGameTree(gameTree);
+    const docId = await uploadSgf(sgf, currentUser);
+    if (docId) history.push(`/view/${docId}`, { afterSave: true });
+  };
 
   return (
     <>
       <Prompt
         when={editMode}
-        message="You have unsaved changes, are you sure you want to leave?"
+        message={(location: Location<LocationState>) =>
+          location.state.afterSave ||
+          'You have unsaved changes, are you sure you want to leave?'
+        }
       />
-      <Fab highlighted={editMode} onClick={() => dispatch(startEditing())}>
+      <Fab highlighted={editMode} onClick={editMode ? handleSave : handleEdit}>
         {editMode ? <Save /> : <Edit />}
       </Fab>
     </>
