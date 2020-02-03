3abeb152294495580b9bc4bc48788b637392a17e
airjp73 <aaron.pettengill.violin@gmail.com>
2020-02-02 21:53:39 -0500

    Modify app version when creating an sgf

--- src/goban/parseSgf/createSgfFromGameTree.test.ts
+++ src/goban/parseSgf/createSgfFromGameTree.test.ts
@@ -1,10 +1,10 @@
 import parseSgf from './parseSgf';
 import normalizeGameTree from './normalizeGameTree';
-import createSgfFromGameTree from './createSgfFromGameTree';
+import createSgfFromGameTree, { APP_STRING } from './createSgfFromGameTree';
 
 const test1 = `
 (
-  ;GM[1]FF[4]
+  ;GM[1]FF[4]AP[${APP_STRING}]
   ;B[aa]
   ;W[ab]
   (
@@ -25,13 +25,15 @@ const test1 = `
 
 const test2 = `
 (
-  ;GM[1]FF[4]
+  ;GM[1]FF[4]AP[${APP_STRING}]
   ;B[aa]
   ;W[ab]
   ;B[aa]C[Hey man, how's it going?
 I didn't see you at the thing yesterday.]
   ;W[ab]C[I know, I was having trouble with these escape characters \\[\\]]
 )`.trim();
+
+const test3 = test2.replace(APP_STRING, 'Cgoban:3');
 
 describe('createSgfFromGameTre', () => {
   it('should reproduce sgf with variations', () => {
@@ -41,6 +43,11 @@ describe('createSgfFromGameTre', () => {
 
   it('should reproduce sgf with comment shenanigans', () => {
     const tree = normalizeGameTree(parseSgf(test2)[0]);
+    expect(createSgfFromGameTree(tree)).toEqual(test2);
+  });
+
+  it('should set the correct app version', () => {
+    const tree = normalizeGameTree(parseSgf(test3)[0]);
     expect(createSgfFromGameTree(tree)).toEqual(test2);
   });
 });
--- src/goban/parseSgf/createSgfFromGameTree.ts
+++ src/goban/parseSgf/createSgfFromGameTree.ts
@@ -1,6 +1,10 @@
 import { GameTree, GameTreeNode } from './normalizeGameTree';
 import { NodeProperties } from './parseSgf';
 
+export const APP_NAME = '0.0.1';
+export const APP_VERSION = 'Kifu.io';
+export const APP_STRING = `${APP_NAME}:${APP_VERSION}`;
+
 function createSgfFromGameTree(gameTree: GameTree): string {
   function traverse(node: GameTreeNode, parenTabs: string): string {
     const moveTabs = `${parenTabs}  `;
@@ -32,7 +36,18 @@ function createSgfFromGameTree(gameTree: GameTree): string {
     return `${parenTabs}(${moves}\n${parenTabs})`;
   }
 
-  return traverse(gameTree.nodes[gameTree.rootNode], '');
+  // The AP node is used to signify the application used to create the sgf
+  // Since we're creating the sgf now, we should change it
+  const rootNode = gameTree.nodes[gameTree.rootNode];
+  const rootNodeWithApp: GameTreeNode = {
+    ...rootNode,
+    properties: {
+      ...(rootNode.properties ?? {}),
+      AP: [APP_STRING],
+    },
+  };
+
+  return traverse(rootNodeWithApp, '');
 }
 
 function stringifyProperties(props: NodeProperties): string {
