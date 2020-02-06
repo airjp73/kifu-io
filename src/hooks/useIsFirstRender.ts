1fe103f439704ac42e303e649bad08fa01b27f10
airjp73 <aaron.pettengill.violin@gmail.com>
2020-02-05 21:59:32 -0500

    Add deprecation flag

--- src/hooks/useIsFirstRender.ts
+++ src/hooks/useIsFirstRender.ts
@@ -1,5 +1,8 @@
 import { useRef, useEffect } from 'react';
 
+/**
+ * @deprecated This is not a good pattern
+ */
 const useIsFirstRender = () => {
   const isFirstRender = useRef(true);
   useEffect(() => {
