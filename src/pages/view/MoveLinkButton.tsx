a05157350d738b3d7a09f82efa69de7cfa619ee6
airjp73 <aaron.pettengill.violin@gmail.com>
2020-02-08 14:48:15 -0500

    Copy the link to the clipboard

--- src/pages/view/MoveLinkButton.tsx
+++ src/pages/view/MoveLinkButton.tsx
@@ -4,21 +4,31 @@ import SpeedDialOption from 'components/SpeedDialOption';
 import { useGoGameContext } from 'goban/GoGameContext';
 import constructMoveLinkString from './constructMoveLinkString';
 import { toast } from 'react-toastify';
+import copyToClipboard from './copyToClipboard';
 
 type MoveLinkButtonProps = Omit<
   React.ComponentProps<typeof SpeedDialOption>,
   'label' | 'onClick'
 >;
 
+const withFullLink = (param: string): string => {
+  const { origin, pathname } = document.location;
+  return `${origin}${pathname}?move=${param}`;
+};
+
 const MoveLinkButton: React.FC<MoveLinkButtonProps> = props => {
   const { gameState, gameTree } = useGoGameContext();
 
   const copyMoveLink = () => {
     try {
-      const moveLink = constructMoveLinkString(gameTree, gameState.node);
-      toast.success(moveLink, { containerId: 'game-view' });
-    } catch (error) {
-      toast.error('Failed to create link to move', {
+      const moveParam = constructMoveLinkString(gameTree, gameState.node);
+      const encodedParam = encodeURIComponent(moveParam);
+      const moveLink = withFullLink(encodedParam);
+      copyToClipboard(moveLink);
+
+      toast.success('Link copied to clipboard', { containerId: 'game-view' });
+    } catch {
+      toast.error('Failed to construct link to current move', {
         containerId: 'game-view',
       });
     }
