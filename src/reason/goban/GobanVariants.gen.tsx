8187e23a26f98cf7df2994a083d1b197d73b0a70
airjp73 <aaron.pettengill.violin@gmail.com>
2020-02-15 19:12:38 -0500

    Commit lingering changes

--- src/reason/goban/GobanVariants.gen.tsx
+++ src/reason/goban/GobanVariants.gen.tsx
@@ -2,15 +2,5 @@
 /* eslint-disable import/first */
 
 
-const $$toRE447283240: { [key: string]: any } = {"Black": 0, "White": 1};
-
-// tslint:disable-next-line:no-var-requires
-const GobanVariantsBS = require('./GobanVariants.bs');
-
 // tslint:disable-next-line:interface-over-type-literal
 export type stoneColor = "Black" | "White";
-
-export const stoneColorString: (color:stoneColor) => string = function (Arg1: any) {
-  const result = GobanVariantsBS.stoneColorString($$toRE447283240[Arg1]);
-  return result
-};
