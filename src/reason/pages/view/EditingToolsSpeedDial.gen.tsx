f89f1ce283c6d474c4325c296f79972abcb6436e
airjp73 <aaron.pettengill.violin@gmail.com>
2020-02-15 19:43:50 -0500

    Rough in EditingToolsSpeedDial

---
+++ src/reason/pages/view/EditingToolsSpeedDial.gen.tsx
@@ -0,0 +1,15 @@
+/* TypeScript file generated from EditingToolsSpeedDial.re by genType. */
+/* eslint-disable import/first */
+
+
+import * as React from 'react';
+
+// tslint:disable-next-line:no-var-requires
+const EditingToolsSpeedDialBS = require('./EditingToolsSpeedDial.bs');
+
+import {direction as SpeedDial_direction} from '../../../../src/reason/common/SpeedDial.gen';
+
+// tslint:disable-next-line:interface-over-type-literal
+export type Props = { readonly children: React.ReactNode; readonly direction?: SpeedDial_direction };
+
+export const make: React.ComponentType<{ readonly children: React.ReactNode; readonly direction?: SpeedDial_direction }> = EditingToolsSpeedDialBS.make;
---
+++ src/reason/pages/view/EditingToolsSpeedDial.re
@@ -0,0 +1,4 @@
+[@gentype]
+[@react.component]
+let make = (~direction, ~children) =>
+  <SpeedDial direction> children </SpeedDial>;
