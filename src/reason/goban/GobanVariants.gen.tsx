5ba70e373ddd0748bd0136e2558fd2f859cf5808
airjp73 <aaron.pettengill.violin@gmail.com>
2020-02-15 19:48:49 -0500

    Remove generated files from source control

--- src/reason/goban/GobanVariants.gen.tsx
+++
@@ -1,6 +0,0 @@
-/* TypeScript file generated from GobanVariants.re by genType. */
-/* eslint-disable import/first */
-
-
-// tslint:disable-next-line:interface-over-type-literal
-export type stoneColor = "Black" | "White";
--- src/reason/util/dateUtils.gen.tsx
+++
@@ -1,8 +0,0 @@
-/* TypeScript file generated from dateUtils.re by genType. */
-/* eslint-disable import/first */
-
-
-// tslint:disable-next-line:no-var-requires
-const dateUtilsBS = require('./dateUtils.bs');
-
-export const monthNames: string[] = dateUtilsBS.monthNames;
