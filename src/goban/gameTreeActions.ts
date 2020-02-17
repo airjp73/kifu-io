d14a3fdc695a4df46b9a2053072c9fce1160661a
airjp73 <aaron.pettengill.violin@gmail.com>
2020-02-17 12:44:29 -0500

    Use addNode action when needing to add a node

--- src/goban/gameTreeActions.ts
+++ src/goban/gameTreeActions.ts
@@ -4,6 +4,7 @@ import { NodeProperties } from './parseSgf/parseSgf';
 import processNodeProperties from './processNode';
 import { pushHistory, setNode, setPoint } from './actions';
 import { stoneColor } from 'reason/goban/GobanVariants.gen';
+import { StoneColor } from './Goban';
 
 export const ADD_NODE = 'game-tree/addNode';
 export const DELETE_BRANCH = 'game-tree/deleteBranch';
@@ -48,19 +49,25 @@ export const addMove = (point: string): ThunkAction<GameStateWithHistory> => (
 
 export interface EditPointAction {
   type: typeof EDIT_POINT;
-  payload: { point: string; color: stoneColor };
+  payload: { point: string; color: StoneColor };
 }
 export const editPoint = (
   point: string,
-  color: stoneColor
+  color: StoneColor
 ): ThunkAction<GameStateWithHistory> => (dispatch, getState) => {
   const currentNode = getState().gameTree.nodes[getState().node];
-  if (currentNode.properties.B || currentNode.properties.W)
+  if (currentNode.properties.B || currentNode.properties.W) {
     dispatch(pushHistory());
-  dispatch({
-    type: EDIT_POINT,
-    payload: { point, color },
-  } as EditPointAction);
+    dispatch({
+      type: ADD_NODE,
+      payload: color === 'b' ? { AB: [point] } : { AW: [point] },
+    } as AddNodeAction);
+  } else {
+    dispatch({
+      type: EDIT_POINT,
+      payload: { point, color },
+    } as EditPointAction);
+  }
 };
 
 export interface DeleteBranchAction {
